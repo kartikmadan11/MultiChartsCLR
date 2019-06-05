ee4aa30505c39eb7cb52892602b90f47da4dc184
Kartik Madan <magiciankartik00@gmail.com>
2019-06-05 10:17:18 +0530

    initial commit

--- MultiChartsCLR/MultiChartsCLR.cpp
+++ MultiChartsCLR/MultiChartsCLR.cpp
@@ -1,4 +1,89 @@
 #include "stdafx.h"
-
 #include "MultiChartsCLR.h"
 
+using System::Runtime::InteropServices::Marshal;
+using MultiChartsCppWrapper::MultiChartsWrapper;
+
+MultiChartsWrapper::MultiChartsWrapper()
+{
+	multiCharts = new MultiCharts();
+}
+
+MultiChartsWrapper::~MultiChartsWrapper()
+{
+	this->multiCharts->DisposeMultiCharts();
+}
+
+void MultiChartsWrapper::SetTrainingData(array<System::Double> ^trainingData)
+{
+	double* trainingDataUnmanaged = new double[trainingData->Length];
+	Marshal::Copy(trainingData, 0, System::IntPtr(trainingDataUnmanaged), trainingData->Length);
+	multiCharts->InitDateArray(trainingData->Length);
+	multiCharts->SetTrainingData(trainingDataUnmanaged);
+	delete trainingDataUnmanaged;
+}
+
+void MultiChartsWrapper::SetDateArray(array<System::String^> ^dateArray)
+{
+	int totalCharLength = dateArray->Length * dateArray[0]->Length;
+	char* dateArrayUnmanaged = new char[totalCharLength];
+	for (int i = 0; i < dateArray->Length; i++)
+	{
+		dateArrayUnmanaged[i] = (char*)(void*)Marshal::StringToHGlobalAnsi(dateArray[i]);
+	}
+	Marshal::FreeHGlobal(dateArrayU);
+/*
+	char*  = new char[dateArray->Length];
+	Marshal::Copy(dateArray, 0, System::IntPtr(dateArrayUnmanaged), dateArray->Length);
+	multiCharts->InitDateArray(dateArray->Length);
+	multiCharts->SetDateArray(dateArrayUnmanaged);
+	delete dateArrayUnmanaged;*/
+}
+
+void MultiChartsWrapper::SetFileName(System::String fileName)
+{
+	char* fileNameUnmanaged = new char[fileName->Length];
+	Marshal::Copy(fileName, 0, System::IntPtr(fileNameUnmanaged), fileName->Length);
+	multiCharts->SetDateArray(fileNameUnmanaged);
+	delete fileNameUnmanaged;
+}
+
+void MultiChartsWrapper::SetLearningRate(double learningRate)
+{
+	multiCharts->SetLearningRate(learningRate);
+}
+
+void MultiChartsWrapper::SetEpochs(int epochs)
+{
+	multiCharts->SetEpochs(epochs);
+}
+
+void MultiChartsWrapper::SetScale(int scale)
+{
+	multiCharts->SetScale(scale);
+}
+
+void MultiChartsWrapper::SetOptimizer(int optimizer)
+{
+	multiCharts->SetOptimizer(optimizer);
+}
+
+void MultiChartsWrapper::SetMomentum(int momentum)
+{
+	multiCharts->SetMomentum(momentum);
+}
+
+void MultiChartsWrapper::SetTestingPart(double testingPart)
+{
+	multiCharts->SetTestingPart(testingPart);
+}
+
+void MultiChartsWrapper::SetTestingWeight(double testingWeight)
+{
+	multiCharts->SetTestingWeight(testingWeight);
+}
+
+double MultiChartsWrapper::TrainModel()
+{
+	return multiCharts->TrainModel();
+}
--- MultiChartsCLR/MultiChartsCLR.h
+++ MultiChartsCLR/MultiChartsCLR.h
@@ -1,10 +1,27 @@
+#include "C:\Users\magic\source\repos\MultiCharts\MultiCharts\MultiCharts.cpp"
+
 #pragma once
 
-using namespace System;
-
-namespace MultiChartsCLR {
-	public ref class Class1
+namespace MultiChartsCppWrapper {
+	public ref class MultiChartsWrapper
 	{
-		// TODO: Add your methods for this class here.
+	public:
+		MultiChartsWrapper();
+		~MultiChartsWrapper();
+
+		void SetTrainingData(array<System::Double> ^trainingData);
+		void SetDateArray(array<System::String^> ^dateArray);
+		void SetFileName(System::String fileName);
+		void SetLearningRate(double learningRate);
+		void SetEpochs(int epochs);
+		void SetScale(int scale);
+		void SetOptimizer(int optimizer);
+		void SetMomentum(int momentum);
+		void SetTestingPart(double testingPart);
+		void SetTestingWeight(double testingWeight);
+		double TrainModel();
+
+	private:
+		MultiCharts *multiCharts;
 	};
 }
